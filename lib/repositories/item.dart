b71688e557e3d911f7ff241875a504506f6cf914
Fransisco Wijaya <fransiscowijaya1999@gmail.com>
2025-06-10 17:33:13 +0700

    thanks

--- lib/repositories/item.dart
+++ lib/repositories/item.dart
@@ -5,12 +5,10 @@ import 'package:partie/utils/string_builder.dart';
 class PartItemData {
   const PartItemData({
     required this.partId,
-    required this.vehicleId,
     this.description = ''
   });
 
   final int partId;
-  final int vehicleId;
   final String description;
 }
 
@@ -73,82 +71,54 @@ class ItemRepository {
       .update((f) => f(name: Value(name), description: Value(description)));
   }
 
-  static Future<String> _getPartPath(int id) async {
+  static Future<List<String>> _getPartPath(int id) async {
     return await db.transaction(() async {
-      print('pret');
-      final part = await db.managers.parts.filter((f) => f.id.equals(id)).getSingle();
+      final part = await db.managers.parts
+        .filter((f) => f.id.equals(id))
+        .getSingle();
 
       if (part.parentId != null) {
-        final basePath = await _getPartPath(part.parentId!);
+        final basePaths = await _getPartPath(part.parentId!);
 
-        return StringBuilder.titleBuilder(basePath, part.name);
+        return basePaths.map((path) => StringBuilder.titleBuilder(path, part.name)).toList();
       } else {
-        print(await db.managers.partVehicles
+        final partVehicles = await db.managers.partVehicles
           .withReferences(
             (prefetch) => prefetch(vehicleId: true)
           )
-          .filter((f) => f.partId.id.equals(part.id)).get());
-        final partVehicle = await db.managers.partVehicles
-          .withReferences(
-            (prefetch) => prefetch(vehicleId: true)
-          )
-          .filter((f) => f.partId.id.equals(part.id)).getSingle();
+          .filter((f) => f.partId.id.equals(id))
+          .map((row) => row.$2.vehicleId.prefetchedData!.first.name)
+          .get();
         
-        return StringBuilder.titleBuilder(partVehicle.$2.vehicleId.prefetchedData!.first.name, part.name);
+        return partVehicles.map((name) => StringBuilder.titleBuilder(name, part.name)).toList(); 
       }
     });
   }
 
   static Future<List<ItemPath>> getItemLinkedPart(int id) async {
-    // final sql =
-    //   'SELECT parts.id, part_vehicles.vehicle_id, part_items.description FROM parts'
-    //   ' CROSS JOIN part_items ON part_items.part_id = parts.id'
-    //   ' CROSS JOIN part_vehicles ON part_vehicles.part_id = parts.id'
-    //   ' WHERE part_vehicles.part_id IN (SELECT part_id FROM part_items WHERE item_id = ?)'
-    //   ' OR part_vehicles.part_id IN (SELECT part_id FROM part_items WHERE item_id = ?)'
-    // ;
+    final parts = await db.managers.partItems
+      .filter((f) => f.itemId.id.equals(id))
+      .get();
 
-    final sql =
-      'SELECT parts.id, part_items.description FROM parts'
-      ' INNER JOIN part_items ON part_items.part_id = parts.id'
-      ' LEFT JOIN part_vehicles ON part_vehicles.part_id = parts.id'
-      ' WHERE part_items.part_id IN (SELECT part_id FROM part_items WHERE item_id = ?)'
-    ;
+    final List<ItemPath> paths = [];
 
-    return await db.transaction(() async {
-      final parts = await db.customSelect(sql, variables: [
-          Variable.withInt(id),
-          // Variable.withInt(id),
-        ])
-        .map((row) {
-          print(row.data['id']);
+    await Future.forEach(parts, (part) async {
+      final partId = part.partId;
+      final partDescription = part.description;
 
-          return row.data['id'];
-          // return PartItemData(
-          //   partId: row.data['id'],
-          //   vehicleId: row.data['vehicle_id'],
-          //   description: row.data['description']
-          // );
-        })
-        .get();
+      final pathList = await _getPartPath(partId);
+      print('safe');
 
-      final List<ItemPath> paths = [];
-
-      await Future.forEach(parts, (part) async {
-        final partId = part.partId;
-        final partDescription = part.description;
-
-        final path = await _getPartPath(partId);
-        print('safe');
+      for (final path in pathList) {
         final toAdd = ItemPath(
           path: path,
           description: partDescription
         );
 
         paths.add(toAdd);
-      });
-
-      return paths;
+      }
     });
+
+    return paths;
   }
 }
