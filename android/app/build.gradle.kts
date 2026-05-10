94f92aa0e9835ca34dcbb9750501bacc23c898a5
Fransisco Wijaya <bidipeppercrap@outlook.com>
2026-05-10 21:34:40 +0700

    ready to publish

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.partie"
+    namespace = "com.wijayamotor.partie"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
